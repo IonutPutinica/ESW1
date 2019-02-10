6bab18d21636d89f43d954d923fff0ef12ad0cba
Ionut <ionutputinicabz@gmail.com>
2019-02-10 15:45:27 +0100

    Added exercise 1-1

---
+++ ExSession1/ex1.1/ex1-2/ex1-2/Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	/*once an integer is added to the printing process, %d needs to be added, 
+	in order for the compiler to recongize that what is next to be printed is an integer*/
+	int c = 5;
+	printf("The integer is %d\n", c);
+
+	while (1);
+	return 0;
+}
